c07ef7616edc2d1ca51a1ffd8da0861731169c61
João Pedro <jpedro1580@gmail.com>
2022-12-31 16:30:53 -0300

    Add limit parameter to user service

--- sharenergy-challenge/packages/backend/src/app/services/user.service.ts
+++ sharenergy-challenge/packages/backend/src/app/services/user.service.ts
@@ -5,7 +5,7 @@ import UserRepository from '../repositories/user-repository';
 export default class UserService {
   constructor(private readonly userRepository: UserRepository) {}
 
-  async getUsers(page?: number) {
-    return await this.userRepository.getUsers(page);
+  async getUsers(page: number, limit: number) {
+    return await this.userRepository.getUsers(page, limit);
   }
 }
