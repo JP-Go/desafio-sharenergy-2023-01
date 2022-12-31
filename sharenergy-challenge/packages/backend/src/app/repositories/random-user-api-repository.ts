5f82164117cee448ad20b287cc5a749243151737
João Pedro <jpedro1580@gmail.com>
2022-12-31 16:30:35 -0300

    Add limit parameter to getUsers method on user repository and implement it on random user api repository

--- sharenergy-challenge/packages/backend/src/app/repositories/random-user-api-repository.ts
+++ sharenergy-challenge/packages/backend/src/app/repositories/random-user-api-repository.ts
@@ -10,13 +10,13 @@ export class RandomUserApiRepository implements UserRepository {
   private DEFAULT_NUMBER_OF_RESULTS = 30;
   private SEED = 'random_seed';
 
-  async getUsers(page?: number) {
+  async getUsers(page?: number, limit?: number) {
     const apiUsers = (
       await (
         await fetch(
           `${this.RANDOM_USER_API_BASE_URL}?seed=${this.SEED}&results=${
-            this.DEFAULT_NUMBER_OF_RESULTS
-          }&page=${page !== undefined ? page : 1}`
+            limit ?? this.DEFAULT_NUMBER_OF_RESULTS
+          }&page=${page ?? 1}`
         )
       ).json()
     ).results as RandomApiUser[];
