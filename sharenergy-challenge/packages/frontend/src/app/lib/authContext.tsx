9ab2f82bb509a94a5215d485dd5d22835e1f194d
João Pedro <jpedro1580@gmail.com>
2023-01-02 12:29:23 -0300

    Refactor auth context manager

---
+++ sharenergy-challenge/packages/frontend/src/app/lib/authContext.tsx
@@ -0,0 +1,59 @@
+import { createContext, ReactNode, useContext, useReducer } from 'react';
+
+type AuthState = { loggedIn: boolean };
+type AuthActions = { kind: 'login' | 'logout'; args?: AuthData };
+type AuthData = { username: string; password: string };
+type AuthContextInterface = {
+  authState: AuthState;
+  login: (username: string, password: string) => void;
+  logout: () => void;
+};
+
+function reducer(state: AuthState, action: AuthActions) {
+  const { kind, args } = action;
+  switch (kind) {
+    case 'login':
+      if (state.loggedIn) {
+        return state;
+      }
+      if (!args) {
+        throw 'Login arguments not passed';
+      }
+      //TODO: Remove dummy credentials
+      const { password, username } = args;
+      if (username == 'test' && password == 'test')
+        return { ...state, loggedIn: true };
+      return { ...state, loggedIn: false };
+    case 'logout':
+      return { ...state, loggedIn: false };
+    default:
+      return state;
+  }
+}
+
+const dummyContext: AuthContextInterface = {
+  login: () => {},
+  logout: () => {},
+  authState: {
+    loggedIn: true,
+  },
+};
+
+export const AuthContext = createContext<AuthContextInterface>(dummyContext);
+
+export function AuthProvider({ children }: { children: ReactNode }) {
+  const [authState, dispatch] = useReducer(reducer, { loggedIn: false });
+  function login(username: string, password: string) {
+    dispatch({ kind: 'login', args: { username, password } });
+  }
+  function logout() {
+    dispatch({ kind: 'logout' });
+  }
+  return (
+    <AuthContext.Provider value={{ authState, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export const useAuth = () => useContext(AuthContext);
