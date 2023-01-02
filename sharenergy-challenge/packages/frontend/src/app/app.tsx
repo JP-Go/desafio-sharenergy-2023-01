5b2a3a41e601f0e0315044ad6864af033bb04724
João Pedro <jpedro1580@gmail.com>
2023-01-02 17:47:58 -0300

    Add queryClient and QueryProvider in the root of the app

--- sharenergy-challenge/packages/frontend/src/app/app.tsx
+++ sharenergy-challenge/packages/frontend/src/app/app.tsx
@@ -1,15 +1,20 @@
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { Route, Routes } from 'react-router-dom';
 import { AuthProvider } from './lib/authContext';
 import Home from './pages/home';
 import Login from './pages/login';
 
+const queryClient = new QueryClient();
+
 export default function App() {
   return (
     <AuthProvider>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
+      <QueryClientProvider client={queryClient}>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </QueryClientProvider>
     </AuthProvider>
   );
 }
