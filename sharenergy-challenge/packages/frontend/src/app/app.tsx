bf7c84f69f4ebe1c0b3d5779b93c070a79cc475f
João Pedro <jpedro1580@gmail.com>
2023-01-02 22:26:27 -0300

    Lazy load homepage component

--- sharenergy-challenge/packages/frontend/src/app/app.tsx
+++ sharenergy-challenge/packages/frontend/src/app/app.tsx
@@ -1,9 +1,10 @@
+import { lazy } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { Route, Routes } from 'react-router-dom';
 import { AuthProvider } from './lib/authContext';
-import Home from './pages/home';
 import Login from './pages/login';
 
+const Home = lazy(() => import('./pages/home'));
 const queryClient = new QueryClient();
 
 export default function App() {
