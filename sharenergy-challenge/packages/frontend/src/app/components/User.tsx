6a057634e87a88083e2380daa2c6511353ab34d0
João Pedro <jpedro1580@gmail.com>
2023-01-02 22:07:29 -0300

    Raise user's fullname size and raise line-spacing

--- sharenergy-challenge/packages/frontend/src/app/components/User.tsx
+++ sharenergy-challenge/packages/frontend/src/app/components/User.tsx
@@ -14,14 +14,14 @@ export default function User({
   username,
 }: UserProps) {
   return (
-    <div className="w-full bg-white mx-auto flex items-center gap-8 p-4 rounded-lg">
+    <div className="w-full bg-white mx-auto flex items-center gap-8 p-4 rounded-lg leading-relaxed">
       <img
         src={pictureUrl}
         alt={pictureUrl}
         className="h-24 w-24 rounded-full outline outline-2 outline-indigo-500 "
       />
       <div>
-        <p className="font-bold">{fullName}</p>
+        <p className="font-bold text-xl">{fullName}</p>
         <p>
           <span className="font-medium">Idade: </span>
           {age}
