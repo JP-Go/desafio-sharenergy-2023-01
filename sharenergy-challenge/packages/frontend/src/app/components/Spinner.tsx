64ec4c69fae7059cefbea30ad91a957e3a252e0e
João Pedro <jpedro1580@gmail.com>
2023-01-04 14:47:09 -0300

    Add a loading spinner component

---
+++ sharenergy-challenge/packages/frontend/src/app/components/Spinner.tsx
@@ -0,0 +1,19 @@
+export default function Spinner() {
+  return (
+    <div className="w-full text-black flex flex-col items-center">
+      <svg
+        className="animate-spin h-20 w-20 mx-auto"
+        viewBox="0 0 24 24"
+        width={12}
+        height={12}
+      >
+        <path
+          d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z"
+          opacity=".25"
+        />
+        <path d="M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z" />
+      </svg>
+      Loading ...
+    </div>
+  );
+}
