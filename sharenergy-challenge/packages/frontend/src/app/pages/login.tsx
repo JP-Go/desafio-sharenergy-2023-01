f2712eeb906bf84fc3f2d63c34739d91e2aa2bc3
João Pedro <jpedro1580@gmail.com>
2023-01-03 23:28:20 -0300

    Fix typo

--- sharenergy-challenge/packages/frontend/src/app/pages/login.tsx
+++ sharenergy-challenge/packages/frontend/src/app/pages/login.tsx
@@ -52,6 +52,7 @@ const Login = () => {
           />
           <div className="flex gap-2 items-center">
             <input name="remeber-me" type="checkbox" ref={rememberRef} />
+            <input name="remember-me" type="checkbox" ref={rememberRef} />
             <label htmlFor="remember-me">Lembre de mim </label>
           </div>
           <button
