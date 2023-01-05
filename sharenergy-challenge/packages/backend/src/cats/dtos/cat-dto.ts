a13705a6a2f31cdbb6d77d8e46793b6de365ea73
João Pedro <jpedro1580@gmail.com>
2023-01-05 16:26:22 -0300

    Refactor Cat module dto to check for null url and correctly map it

--- sharenergy-challenge/packages/backend/src/cats/dtos/cat-dto.ts
+++ sharenergy-challenge/packages/backend/src/cats/dtos/cat-dto.ts
@@ -1,9 +1,14 @@
-import { CatResource } from '../entities/cat-resouce';
+import { CatResource, CatResourceStatus } from '../entities/cat-resouce';
+
+export type CatResourceHttpResponse = {
+  url: string | null;
+  status: CatResourceStatus;
+};
 
 export class CatDto {
-  static toHttp(catResource: CatResource) {
+  static toHttp(catResource: CatResource): CatResourceHttpResponse {
     return {
-      url: catResource.url,
+      url: catResource.url?.toString() ?? null,
       status: catResource.status,
     };
   }
