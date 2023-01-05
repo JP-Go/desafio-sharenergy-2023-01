e68c0d3cf884fd88a3111f5092e6fe592f86ba2e
João Pedro <jpedro1580@gmail.com>
2023-01-05 19:47:08 -0300

    Send 404 code if not status code given is not found

--- sharenergy-challenge/packages/backend/src/cats/http-cats/http-cats.controller.ts
+++ sharenergy-challenge/packages/backend/src/cats/http-cats/http-cats.controller.ts
@@ -1,14 +1,27 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { CatsService } from '../contracts/cats.service';
 import { CatDto } from '../dtos/cat-dto';
+import { Response } from 'express';
+import {
+  Controller,
+  Get,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Res,
+} from '@nestjs/common';
 
 @Controller('cats')
 export class HttpCatsController {
   constructor(private readonly httpCatsService: CatsService) {}
 
   @Get(':code')
-  async getCatByCode(@Param('code', ParseIntPipe) code: number) {
-    const cat = await this.httpCatsService.getCatByCode(code);
-    return CatDto.toHttp(cat);
+  async getCatByCode(
+    @Res({ passthrough: true }) res: Response,
+    @Param('code', ParseIntPipe) code: number
+  ) {
+    const catResource = await this.httpCatsService.getCatByCode(code);
+    if (catResource.status === 'error') res.status(HttpStatus.NOT_FOUND);
+
+    return CatDto.toHttp(catResource);
   }
 }
