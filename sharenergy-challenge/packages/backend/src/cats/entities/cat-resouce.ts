e3c7bc8c00c60a4d760270f0f1678a2524d1ff29
João Pedro <jpedro1580@gmail.com>
2023-01-05 15:56:34 -0300

    Enforce url type safety on cat resource entity

--- sharenergy-challenge/packages/backend/src/cats/entities/cat-resouce.ts
+++ sharenergy-challenge/packages/backend/src/cats/entities/cat-resouce.ts
@@ -1,19 +1,22 @@
 type CatResourceStatus = 'ok' | 'error';
 
 export class CatResource {
-  private _url: string | null;
+  private _url: URL | null;
   private _status: CatResourceStatus;
 
   static withOkStatus(resourceUrl: string) {
     return new CatResource(resourceUrl);
   }
 
-  static withError(resourceUrl: string) {
-    return new CatResource(resourceUrl, 'error');
+  static withError() {
+    return new CatResource(null, 'error');
   }
 
-  constructor(resourceUrl: string, status: CatResourceStatus = 'ok') {
-    this._url = resourceUrl;
+  private constructor(
+    resourceUrl: string | null,
+    status: CatResourceStatus = 'ok'
+  ) {
+    this._url = resourceUrl === null ? null : new URL(resourceUrl);
     this._status = status;
   }
 
