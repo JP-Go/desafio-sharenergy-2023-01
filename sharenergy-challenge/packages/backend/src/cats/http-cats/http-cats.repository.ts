5d433dcc2fdf93a52ab29a0af6084391eb74af2c
João Pedro <jpedro1580@gmail.com>
2023-01-05 11:42:43 -0300

    Provide an implementation of CatsRepository using the HTTPCats api

---
+++ sharenergy-challenge/packages/backend/src/cats/http-cats/http-cats.repository.ts
@@ -0,0 +1,29 @@
+import { HttpService } from '@nestjs/axios';
+import { Logger } from '@nestjs/common';
+import { AxiosError } from 'axios';
+import { catchError, firstValueFrom } from 'rxjs';
+import { CatsRepository } from '../contracts/cats.repository';
+import { CatResource } from '../entities/cat-resouce';
+
+export class HttpCatsRepository implements CatsRepository {
+  private HTTP_CATS_URL = 'https://http.cat';
+
+  constructor(private readonly httpService: HttpService) {}
+
+  async getCatByCode(code: number) {
+    const fullUrl = `${this.HTTP_CATS_URL}/${code}`;
+    const response = await firstValueFrom(
+      this.httpService.get(fullUrl).pipe(
+        catchError((error: AxiosError) => {
+          const logger = new Logger(CatsRepository.name);
+          logger.error(error.response.data);
+          throw 'Error fetching from HTTP.cat api';
+        })
+      )
+    );
+
+    return response.status === 200
+      ? CatResource.withOkStatus(fullUrl)
+      : CatResource.withError(null);
+  }
+}
