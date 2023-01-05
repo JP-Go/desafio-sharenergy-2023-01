ec098b0e0f28a7933dba4d411601e2535df85d51
João Pedro <jpedro1580@gmail.com>
2023-01-05 16:22:09 -0300

    Add tests for Cat dto

---
+++ sharenergy-challenge/packages/backend/src/cats/dtos/cat-dto.spec.ts
@@ -0,0 +1,25 @@
+import { CatResource } from '../entities/cat-resouce';
+import { CatDto } from './cat-dto';
+
+describe('Cat Dto tests', () => {
+  it("should correctly transform a CatResource entity with a 'ok' status to a CatHttpResponse", () => {
+    const result = CatDto.toHttp(
+      CatResource.withOkStatus('https://www.google.com/')
+    );
+    expect(result).toStrictEqual(
+      expect.objectContaining({
+        url: 'https://www.google.com/',
+        status: 'ok',
+      })
+    );
+  });
+  it("should correctly transform a CatResource entity with a 'error' status to a CatHttpResponse", () => {
+    const result = CatDto.toHttp(CatResource.withError());
+    expect(result).toStrictEqual(
+      expect.objectContaining({
+        url: null,
+        status: 'error',
+      })
+    );
+  });
+});
