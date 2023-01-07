f7efe626a957b7363aef744d37000d44a3d44bae
João Pedro <jpedro1580@gmail.com>
2023-01-06 22:46:39 -0300

    Use the base Client entity to type save method and create update method

--- sharenergy-challenge/packages/backend/src/client/client.repository.ts
+++ sharenergy-challenge/packages/backend/src/client/client.repository.ts
@@ -1,11 +1,9 @@
-import { Client, ClientProps } from '../client/entities/client';
+import { Client } from '../client/entities/client';
 
 export abstract class ClientRepository {
   abstract findById: (id: string) => Promise<Client | null>;
   abstract findMany: () => Promise<Client[]>;
-  abstract save: (
-    clientProps: Partial<ClientProps>,
-    id?: string
-  ) => Promise<Client>;
+  abstract save: (client: Client) => Promise<Client>;
+  abstract update: (client: Partial<Client>, id: string) => Promise<Client>;
   abstract delete: (id: string) => Promise<void>;
 }
