70b2139097b70ced97d547e624546ec260ea331f
João Pedro <jpedro1580@gmail.com>
2023-01-06 22:44:10 -0300

    Allow the props to be visible by subclasses

--- sharenergy-challenge/packages/backend/src/client/entities/base.ts
+++ sharenergy-challenge/packages/backend/src/client/entities/base.ts
@@ -1,21 +1,9 @@
 export default class BaseEntity<T> {
   private _id: string | null;
-  private _props: T;
+  protected _props: T;
 
   constructor(props: T, id?: string) {
     this._id = id ?? null;
     this._props = props;
-  }
-
-  public get id() {
-    return this._id;
-  }
-
-  public get props() {
-    return this._props;
-  }
-
-  public set props(newProps: T) {
-    this._props = newProps;
   }
 }
