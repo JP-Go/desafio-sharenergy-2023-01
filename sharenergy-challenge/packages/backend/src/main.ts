16a7b0d974685a9bd0f956cbf8001b60512543cc
João Pedro <jpedro1580@gmail.com>
2023-01-08 16:29:07 -0300

    Allow transforming requests on validation

--- sharenergy-challenge/packages/backend/src/main.ts
+++ sharenergy-challenge/packages/backend/src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   });
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   const port = process.env.PORT || 3333;
   await app.listen(port);
   Logger.log(
