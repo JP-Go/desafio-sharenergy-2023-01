f96135be54bae411b73572fed8189be022ab55de
João Pedro <jpedro1580@gmail.com>
2023-01-08 14:34:32 -0300

    Add MongooseModule to the project root

--- sharenergy-challenge/packages/backend/src/app/app.module.ts
+++ sharenergy-challenge/packages/backend/src/app/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { CatsModule } from '../cats/cats.module';
+import { ClientModule } from '../client/client.module';
 
 @Module({
-  imports: [CatsModule],
+  imports: [
+    CatsModule,
+    ClientModule,
+    MongooseModule.forRoot(
+      process.env.CONNECTION_STRING ??
+        'mongodb://root:password@127.0.0.1:27020/sharenergy?authSource=admin'
+    ),
+  ],
 })
 export class AppModule {}
