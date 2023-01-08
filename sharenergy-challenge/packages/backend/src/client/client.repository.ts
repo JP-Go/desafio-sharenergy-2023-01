aef509d2d613d7b27e449d67f35333124c46720a
João Pedro <jpedro1580@gmail.com>
2023-01-08 20:27:52 -0300

    Refactor ClientRepository contract

--- sharenergy-challenge/packages/backend/src/client/client.repository.ts
+++ sharenergy-challenge/packages/backend/src/client/client.repository.ts
@@ -1,9 +1,14 @@
-import { Client } from '../client/entities/client';
+import { Client } from './entities/client';
+import { AddressProps } from './entities/address';
+import { Replace } from '../helpers/replace';
 
 export abstract class ClientRepository {
   abstract findById: (id: string) => Promise<Client>;
   abstract findMany: () => Promise<Client[]>;
   abstract save: (client: Client) => Promise<Client>;
-  abstract update: (client: Partial<Client>, id: string) => Promise<Client>;
+  abstract update: (
+    client: Replace<Partial<Client>, { address: Partial<AddressProps> }>,
+    id: string
+  ) => Promise<Client>;
   abstract delete: (id: string) => Promise<void>;
 }
