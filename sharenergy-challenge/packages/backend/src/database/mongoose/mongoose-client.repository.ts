82f5bb6f6c6c2b3be7511c6acc28dba8d3fd536e
João Pedro <jpedro1580@gmail.com>
2023-01-08 15:54:31 -0300

    Implement part of the methods of ClientRepository on MongooseClientRepository

--- sharenergy-challenge/packages/backend/src/database/mongoose/mongoose-client.repository.ts
+++ sharenergy-challenge/packages/backend/src/database/mongoose/mongoose-client.repository.ts
@@ -3,6 +3,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
 import { Client } from '../../client/entities/client';
+import { ClientNotFound } from '../../client/errors/client-not-found';
 import { ClientRepository } from '../../client/client.repository';
 import { ClientDocument, Client as ClientModel } from './schemas/client.schema';
 import { ClientMapper } from './mappers/mongoose-client-mapper';
@@ -11,16 +12,28 @@ import { ClientMapper } from './mappers/mongoose-client-mapper';
 export class MongooseClientRepository implements ClientRepository {
   constructor(
     @InjectModel(ClientModel.name)
-    private readonly catModel: Model<ClientDocument>
+    private readonly clientModel: Model<ClientDocument>
   ) {}
 
   async findMany() {
-    const rawClients = await this.catModel.find().exec();
+    const rawClients = await this.clientModel.find().exec();
     return rawClients.map(ClientMapper.toDomain);
   }
 
-  findById: (id: string) => Promise<Client>;
+  async findById(id: string) {
+    const client = await this.clientModel.findById(id).exec();
+    if (!client) {
+      throw new ClientNotFound();
+    }
+    return ClientMapper.toDomain(client);
+  }
   save: (client: Client) => Promise<Client>;
   update: (client: Partial<Client>, id: string) => Promise<Client>;
-  delete: (id: string) => Promise<void>;
+  async delete(id: string) {
+    const exists = await this.clientModel.exists({ id }).exec();
+    if (!exists) {
+      throw new ClientNotFound();
+    }
+    await this.clientModel.deleteOne({ _id: id }).exec();
+  }
 }
