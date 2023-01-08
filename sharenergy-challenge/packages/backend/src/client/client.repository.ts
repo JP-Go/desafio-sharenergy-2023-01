f4cb0d38841a8d368247dccc20daff4aa26480e2
João Pedro <jpedro1580@gmail.com>
2023-01-08 15:53:35 -0300

    Client module returns an client or errors

--- sharenergy-challenge/packages/backend/src/client/client.repository.ts
+++ sharenergy-challenge/packages/backend/src/client/client.repository.ts
@@ -1,7 +1,7 @@
 import { Client } from '../client/entities/client';
 
 export abstract class ClientRepository {
-  abstract findById: (id: string) => Promise<Client | null>;
+  abstract findById: (id: string) => Promise<Client>;
   abstract findMany: () => Promise<Client[]>;
   abstract save: (client: Client) => Promise<Client>;
   abstract update: (client: Partial<Client>, id: string) => Promise<Client>;
