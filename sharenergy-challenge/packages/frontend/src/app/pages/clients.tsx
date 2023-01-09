416ab821d22b9a1e549a44a389baa49b2d92c0ba
João Pedro <jpedro1580@gmail.com>
2023-01-09 15:38:19 -0300

    Create client listing page

--- sharenergy-challenge/packages/frontend/src/app/pages/clients.tsx
+++ sharenergy-challenge/packages/frontend/src/app/pages/clients.tsx
@@ -1,10 +1,61 @@
+import { useQuery } from 'react-query';
+
 import Header from '../components/Header';
 import AuthedPage from './authed-page';
 
+import { fetchClients } from '../services/client-api';
+import { formatCpfString, formatPhoneString } from '../utils/formatters';
+import { useState } from 'react';
+
 export default function Clients() {
+  const { data: clients } = useQuery('get-clients', () => fetchClients());
+  const [query, setQuery] = useState('');
+
+  const filteredClients = clients?.filter(({ name }) =>
+    name.match(new RegExp(query, 'i'))
+  );
+
   return (
     <AuthedPage>
       <Header path="/clients" />
+      <h1 className="text-2xl font-bold mx-auto text-center mt-8">Clientes</h1>
+      <main className="w-4/5 my-8 mx-auto">
+        <div className="mx-auto my-4 flex items-center gap-2">
+          <h3 className="w-max font-semibold">Buscar cliente:</h3>
+          <input
+            className="p-1 rounded-lg outline outline-1 outline-indigo-500"
+            type="text"
+            value={query}
+            onChange={(e) => {
+              setQuery(e.target.value);
+            }}
+          />
+        </div>
+        <table className="table-auto border-collapse w-full mx-auto">
+          <thead className="bg-indigo-500 text-white rounded-lg text-xl">
+            <tr className="w-full">
+              <th>Nome</th>
+              <th>Telefone</th>
+              <th>E-mail</th>
+              <th>CPF</th>
+            </tr>
+          </thead>
+          <tbody>
+            {filteredClients?.map(({ id, name, phone, email, cpf }) => (
+              <tr
+                onClick={() => console.log(`redirecting to /clients/${id}`)}
+                key={id}
+                className="cursor-pointer text-center bg-white border font-normal hover:scale-110 transition-all duration-300"
+              >
+                <td className="py-2">{name}</td>
+                <td className="py-2">{formatPhoneString(phone)}</td>
+                <td className="py-2">{email}</td>
+                <td className="py-2">{formatCpfString(cpf)}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </main>
     </AuthedPage>
   );
 }
