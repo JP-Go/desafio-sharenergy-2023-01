89478a5e4242f6e3e4434586c08e03d4e3fb8882
João Pedro <jpedro1580@gmail.com>
2023-01-09 19:58:18 -0300

    Refetch data on mount on client listing

--- sharenergy-challenge/packages/frontend/src/app/pages/clients.tsx
+++ sharenergy-challenge/packages/frontend/src/app/pages/clients.tsx
@@ -9,7 +9,9 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 export default function Clients() {
-  const { data: clients } = useQuery('get-clients', () => fetchClients());
+  const { data: clients } = useQuery('get-clients', () => fetchClients(), {
+    refetchOnMount: true,
+  });
   const [query, setQuery] = useState('');
   const navigate = useNavigate();
 
