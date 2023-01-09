12b04ddf76dc014543296b98b823cbe5bcc1943c
João Pedro <jpedro1580@gmail.com>
2023-01-09 18:59:26 -0300

    Extend card functionality

--- sharenergy-challenge/packages/frontend/src/app/components/Card.tsx
+++ sharenergy-challenge/packages/frontend/src/app/components/Card.tsx
@@ -4,11 +4,10 @@ export interface CardProps extends HTMLAttributes<HTMLDivElement> {
   children: ReactNode;
 }
 
-export default function Card({ children, className }: CardProps) {
+export default function Card(props: CardProps) {
+  const { className, children, ...rest } = props;
   return (
-    <div
-      className={`w-full bg-white mx-auto flex items-center gap-8 rounded-lg leading-relaxed ${className}`}
-    >
+    <div className={`bg-white w-full rounded-lg ${className}`} {...rest}>
       {children}
     </div>
   );
