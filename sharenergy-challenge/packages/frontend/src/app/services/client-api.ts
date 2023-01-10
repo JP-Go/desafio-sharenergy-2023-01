fd9c46cd06413e446fa5fc1dd6c6bb1915bdaeb4
João Pedro <jpedro1580@gmail.com>
2023-01-10 00:52:30 -0300

    Create client api service functions to use throughout the app

--- sharenergy-challenge/packages/frontend/src/app/services/client-api.ts
+++ sharenergy-challenge/packages/frontend/src/app/services/client-api.ts
@@ -10,10 +10,35 @@ export async function fetchClients(service: AxiosInstance = clientApiService) {
   return clients.data;
 }
 
-export async function sendForm(
+export async function fetchClientData(
+  id: string,
+  service: AxiosInstance = clientApiService
+) {
+  const result = await service.get<HttpClientResponse>(id);
+  return result.data;
+}
+
+export async function createClient(
   formData: any,
   service: AxiosInstance = clientApiService
 ) {
   const res = await service.post('', formData);
+  return res;
+}
+
+export async function patchClient(
+  id: string,
+  data: any,
+  service: AxiosInstance = clientApiService
+) {
+  const res = await service.patch(`${id}`, data);
+  return res;
+}
+
+export async function deleteClient(
+  id: string,
+  service: AxiosInstance = clientApiService
+) {
+  const res = await service.delete(`${id}`);
   return res;
 }
