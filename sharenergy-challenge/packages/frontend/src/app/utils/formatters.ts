018b49b1ba148c9ea2c560958d175acebff89931
João Pedro <jpedro1580@gmail.com>
2023-01-10 00:51:53 -0300

    Refactor formatters

--- sharenergy-challenge/packages/frontend/src/app/utils/formatters.ts
+++ sharenergy-challenge/packages/frontend/src/app/utils/formatters.ts
@@ -2,21 +2,19 @@ export function formatPhoneString(phoneString: string) {
   const dddCode = phoneString.slice(0, 2);
   const firstDigits = phoneString.slice(2, 7);
   const lastDigits = phoneString.slice(7, phoneString.length);
-  return `(${dddCode}) ${firstDigits}-${lastDigits} `;
+  return `(${dddCode})${firstDigits}-${lastDigits}`;
 }
 
 export function formatCpfString(cpfString: string) {
-  const firstDigits = cpfString
-    .slice(0, 9)
-    .split(/(\d\d\d)/)
-    .filter((group) => group !== '')
-    .join('.');
+  const firstTrio = cpfString.slice(0, 3);
+  const secondTrio = cpfString.slice(3, 6);
+  const thirdTrio = cpfString.slice(6, 9);
   const verifiers = cpfString.slice(9, 12);
-  return `${firstDigits}-${verifiers}`;
+  return `${firstTrio}.${secondTrio}.${thirdTrio}-${verifiers}`;
 }
 
 export function formatCepString(cepString: string) {
-  const sector = cepString.slice(0, 6);
-  const distribution = cepString.slice(6, 9);
-  return `${sector}-${distribution} `;
+  const sector = cepString.slice(0, 5);
+  const distribution = cepString.slice(5, cepString.length);
+  return `${sector}-${distribution}`;
 }
